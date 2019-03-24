cc401be68d08d99f0b16639aa9266f5475e84d12
jas-dev <jaysamora@outlook.com>
2019-03-23 18:07:25 -0700

    finished handleGetDataSuccess adjusted index and css to make containers for carousel dom creation

--- assets/js/yelp.js
+++ assets/js/yelp.js
@@ -38,74 +38,48 @@ class Yelp{
 	handleGetDataSuccess(response) {
 		console.log("response:" ,response);
 
-		var results = $('<div>').addClass("responseContainer");
-        var resultsTwo = $('<div>').addClass("responseContainer");
-        var resultsThree = $('<div>').addClass("responseContainer");
+        var resultsObj = {
+            firstResult: $('<div>').addClass("resultContainer"),
+            secondResult: $('<div>').addClass("resultContainer"),
+            thirdResult: $('<div>').addClass("resultContainer")
+        };
 
         //from response, retrieve name, image/imageLink, phone, number of reviews, rating, location, price; append to carousel dom elements
         //loop to retrieve
 
+        for(var biz = 0; biz <= 2; biz++){
+            //pull all the desired response values for current biz and store in variables
+            //create dom elements using the response values
+           var name = $("<div>").text(response["businesses"][biz]["name"]);
+           console.log(name)
+           var pic = $("<img>").attr({
+                src: response["businesses"][biz]['image_url'],
+                alt: response["businesses"][biz]['name']+" "+"photo",
+                width: 250,
+                height: 165.75,
+            });
+           var picLink = $("<a>").attr({href: response["businesses"][biz]['url']}).append(pic); //add this to dom(not bizURL or pic)
+           var phone = $("<div>").text("Phone: "+response["businesses"][biz]['display_phone']);
+           var review = $("<div>").text("Reviews: "+response["businesses"][biz]['review_count']);
+           var rating = $("<div>").text("Rating: "+response["businesses"][biz]['rating']+" Stars");
+           var addy = $("<div>").text(response["businesses"][biz]['location']['display_address']);
+           var price = $("<div>").text("Pricing: "+response["businesses"][biz]['price']);
+           ////append to respective results container
+           if(biz === 0){
+               resultsObj["firstResult"].append(name, picLink, phone, review, rating, addy, price);
+               $('.resultOne').append(resultsObj["firstResult"]);
+           }else if(biz === 1){
+               resultsObj["secondResult"].append(name, picLink, phone, review, rating, addy, price);
+               $('.resultTwo').append(resultsObj["secondResult"]);
+           }else{
+               resultsObj["thirdResult"].append(name, picLink, phone, review, rating, addy, price);
+               $('.resultThree').append(resultsObj["thirdResult"]);
+           }
 
+           //increment biz
+        }
 
-        /*var name = $("<div>").text(response["businesses"][0]['name']);
-        var nameTwo =$("<div>").text(response["businesses"][1]['name']);
-        var nameThree = $("<div>").text(response["businesses"][2]['name']);
-
-        var image = $("<img>").attr({
-            src: response["businesses"][0]['image_url'],
-            alt: response["businesses"][0]['name']+" "+"photo",
-            width: 250,
-            height: 165.75,
-        });
-
-        var imageTwo = $("<img>").attr({
-            src: response["businesses"][1]['image_url'],
-            alt: response["businesses"][1]['name']+" "+"photo",
-            width: 250,
-            height: 165.75,
-        });
-
-        var imageThree = $("<img>").attr({
-            src: response["businesses"][2]['image_url'],
-            alt: response["businesses"][2]['name']+" "+"photo",
-            width: 250,
-            height: 165.75,
-        });
-
-        //var url = $("<div>").text(response["businesses"][0]['url']);
-        var imageLink = $("<a>").attr({href: response["businesses"][0]['url']}).append(image);
-        var imageLinkTwo = $("<a>").attr({href: response["businesses"][1]['url']}).append(imageTwo);
-        var imageLinkThree = $("<a>").attr({href: response["businesses"][2]['url']}).append(imageThree);
-
-        var phone = $("<div>").text(response["businesses"][0]['display_phone']);
-        var phoneTwo = $("<div>").text(response["businesses"][1]['display_phone']);
-        var phoneThree = $("<div>").text(response["businesses"][2]['display_phone']);
-
-        var reviews = $("<div>").text("Reviews: "+response["businesses"][0]['review_count']);
-        var reviewsTwo = $("<div>").text("Reviews: "+response["businesses"][1]['review_count']);
-        var reviewsThree = $("<div>").text("Reviews: "+response["businesses"][2]['review_count']);
-
-        var rating = $("<div>").text("Rating: "+response["businesses"][0]['rating']+" stars");
-        var ratingTwo = $("<div>").text("Rating: "+response["businesses"][1]['rating']+" stars");
-        var ratingThree = $("<div>").text("Rating: "+response["businesses"][2]['rating']+" stars");
-
-        var location = $("<div>").text(response["businesses"][0]['location']['display_address']);
-        var locationTwo = $("<div>").text(response["businesses"][1]['location']['display_address']);
-        var locationThree = $("<div>").text(response["businesses"][2]['location']['display_address']);
-
-        var price = $("<div>").text("Price: "+response["businesses"][0]['price']);
-        var priceTwo = $("<div>").text("Price: "+response["businesses"][1]['price']);
-        var priceThree = $("<div>").text("Price: "+response["businesses"][2]['price']);*/
-
-        results.append(name, imageLink, phone, reviews, rating, location, price);
-        resultsTwo.append(nameTwo, imageLinkTwo, phoneTwo, reviewsTwo, ratingTwo, locationTwo, priceTwo);
-        resultsThree.append(nameThree, imageLinkThree, phoneThree, reviewsThree, ratingThree, locationThree, priceThree);
-
+        //when response dom elements have been created and added to carousel slides, remove hide class from modal to make it appear and display the carousel
         $('.modal').toggleClass('hide');
-
-
-        $('.slide1').append(results);
-        $('.slide2').append(resultsTwo);
-        $('.slide3').append(resultsThree);
 	}
 }
