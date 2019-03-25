fb5f00a12e5d931d5decde1158dd4e2bf35895d9
jenOng19 <jennifer.sy.ong@gmail.com>
2019-03-25 10:51:24 -0700

    added background image on main page

--- assets/js/searchCategory.js
+++ assets/js/searchCategory.js
@@ -13,29 +13,48 @@ class SearchCategory{
 
     addEventHandlers(){
         $('#recipeButton').click(this.handleRecipeAndYelpButton);
-        $('#yelpButton').click(this.handleRecipeAndYelpButton);
         $('#recipeButton').click(this.handleSearchMode);
+        $('#yelpButton').click(this.handleRecipeAndYelpButton);
         $('#foodSearchBar').click(this.handleFoodSearchBar);
         $('#drinkSearchBar').click(this.handleDrinkSearchBar);
         $('#searchButton').click(this.handleSearch);
-		
     }
+
+    //=============================================================================================================
+    // When either "Eat Out" or "Cook" button are clicked, the main page will hide and the search page will appear
+    //=============================================================================================================
 
     handleRecipeAndYelpButton(){
         $('.input').toggleClass('hide');
         $('.main').toggleClass('hide');
     }
 
-   
+    //=============================================================================================================
+    // Gets the value inputed into the food search bar and returns that value wherever the function is called
+    //=============================================================================================================
+
     handleFoodSearchBar(){
         var userInput=this.domElements.foodInput.val();
         return userInput;
     }
 
+    //=============================================================================================================
+    // Gets the value inputed into the drink search bar and returns that value wherever the function is called
+    //=============================================================================================================
+
     handleDrinkSearchBar(){
         var userInput=this.domElements.drinkInput.val();
         return userInput;
     }
+
+    //=============================================================================================================
+    // Gets the value inputed into the food and drink search bar,
+    // Checks if user clicked on "Go out" or "Cook at Home",
+    // If search mode is "out", food and drink values will be passed into Yelp call,
+    // If search mode is "home", check if:
+    // No drink value= pass in food value to mealDB to get recipes
+    // No food value= pass in drink value to cocktails and beer database to get recipes
+    //=============================================================================================================
     
     handleSearch(){
         var foodValue=this.handleFoodSearchBar();
@@ -62,6 +81,10 @@ class SearchCategory{
             }
         }
     }
+
+    //=============================================================================================================
+    // When "cook" button is clicked, handleSearchMode will be called and searchMode will swith to "home"
+    //=============================================================================================================
 
     handleSearchMode(){
         this.searchMode='home';
